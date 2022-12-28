650387cf2c8caf098896dc18972f091f8fdf390a
Юлия К <kazuta.artem@gmail.com>
2022-12-29 02:17:04 +0300

    dqd

--- js/picture-form.js
+++ js/picture-form.js
@@ -10,8 +10,8 @@ const photoPreview = photoUploadForm.querySelector('.img-upload__preview');
 const photoEffectsPreview = photoUploadForm.querySelectorAll('.effects__preview');
 const closeButton = document.querySelector('#upload-cancel');
 const textarea = document.querySelector('.text__description');
-const counter = document.querySelector('.counter-block__dinamic-number');
 const hashtags = document.querySelector('.text__hashtags');
+const fieldSlider = document.querySelector('.img-upload__effect-level');
 
 const acceptPhtots = ['.jpg', '.jpeg', '.png', '.gif'];
 if (acceptPhtots) {
@@ -33,21 +33,33 @@ function openPhoto() {
   photoOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
   validateForm();
-  textarea.addEventListener('input', () => {
-    counter.textContent = textarea.value.length;
-  });
   document.addEventListener('keydown', onEscKeydown);
 }
 
 function closePhoto() {
   photoOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
+  fieldSlider.classList.add('hidden');
   photoUploadForm.reset();
   document.removeEventListener('keydown', onEscKeydown);
 }
 
 closeButton.addEventListener('click', () => {
   closePhoto();
+});
+
+const sliderElement = document.querySelector('.effect-level__slider');
+const valueElement = document.querySelector('.effect-level__value');
+valueElement.value = 100;
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  step: 1,
+  connect: 'lower',
 });
 
 photoFile.addEventListener('change', (evt) => {
@@ -57,61 +69,97 @@ photoFile.addEventListener('change', (evt) => {
 
   reader.addEventListener('load', (ev) => {
     photoPreview.innerHTML = `<img src="${ev.target.result}">`;
-    const decreaseButton = document.querySelector('.scale__control--smaller');
-    const increaseButton = document.querySelector('.scale__control--bigger');
-    const scaleValue = document.querySelector('.scale__control--value');
-    const pictureScale = photoPreview.querySelector('img');
-    const MAX_VALUE_COUNT = 100;
-
-    scaleValue.value = `${MAX_VALUE_COUNT}%`;
-    let valueScale = MAX_VALUE_COUNT;
-
-    decreaseButton.addEventListener('click', () => {
-      if (scaleValue.value === '25%') {
-        pictureScale.style.transform = `scale(${scaleValue.value = 25}%)`;
-        scaleValue.value = '25%';
-      } else {
-        pictureScale.style.transform = `scale(${scaleValue.value = valueScale -= 25}%)`;
-        scaleValue.value = `${valueScale}%`;
-      }
-    });
-
-    increaseButton.addEventListener('click', () => {
-      if (scaleValue.value === '100%') {
-        pictureScale.style.transform = `scale(${scaleValue.value = 100}%)`;
-        scaleValue.value = '100%';
-      } else {
-        pictureScale.style.transform = `scale(${scaleValue.value = valueScale += 25}%)`;
-        scaleValue.value = `${valueScale}%`;
-      }
-    });
-
-    const sliderElement = document.querySelector('.effect-level__slider');
-    const valueElement = document.querySelector('.effect-level__value');
-    valueElement.style.display = 'block';
-    valueElement.style.color = 'black';
-    valueElement.value = 100;
-
-    noUiSlider.create(sliderElement, {
-      range: {
-        min: 0,
-        max: 100,
-      },
-      start: 100,
-      step: 1,
-      connect: 'lower',
-    });
-
-    sliderElement.noUiSlider.on('update', () => {
-      valueElement.value = sliderElement.noUiSlider.get();
-    });
+    const currentPicture = photoPreview.querySelector('img');
 
     const changeBackPicture = (picture) => {
+      currentPicture.classList.add('effects__preview--none');
+      if (picture.classList[1] === 'effects__preview--none') {
+        fieldSlider.classList.add('hidden');
+      }
       picture.addEventListener('click', () => {
-        pictureScale.classList.remove(pictureScale.classList[0]);
-        pictureScale.classList.add(picture.classList[1]);
+        currentPicture.classList.remove(currentPicture.classList[0]);
+        currentPicture.classList.add(picture.classList[1]);
 
-        // console.log(typeof(picture.classList[1]));
+        if (picture.classList[1] === 'effects__preview--chrome') {
+          fieldSlider.classList.remove('hidden');
+          sliderElement.noUiSlider.on('update', () => {
+            valueElement.value = sliderElement.noUiSlider.get();
+            currentPicture.style.filter = `grayscale(${valueElement.value})`;
+          });
+
+          sliderElement.noUiSlider.updateOptions({
+            range: {
+              min: 0,
+              max: 1,
+            },
+            start: 1,
+            step: 0.1,
+          });
+        } else if (picture.classList[1] === 'effects__preview--sepia') {
+          fieldSlider.classList.remove('hidden');
+          sliderElement.noUiSlider.on('update', () => {
+            valueElement.value = sliderElement.noUiSlider.get();
+            currentPicture.style.filter = `sepia(${valueElement.value})`;
+          });
+
+          sliderElement.noUiSlider.updateOptions({
+            range: {
+              min: 0,
+              max: 1,
+            },
+            start: 1,
+            step: 0.1,
+          });
+        } else if (picture.classList[1] === 'effects__preview--marvin') {
+          fieldSlider.classList.remove('hidden');
+          sliderElement.noUiSlider.on('update', () => {
+            valueElement.value = sliderElement.noUiSlider.get();
+            currentPicture.style.filter = `invert(${`${valueElement.value}%`})`;
+          });
+
+          sliderElement.noUiSlider.updateOptions({
+            range: {
+              min: 0,
+              max: 100,
+            },
+            start: 100,
+            step: 1,
+          });
+        } else if (picture.classList[1] === 'effects__preview--phobos') {
+          fieldSlider.classList.remove('hidden');
+          sliderElement.noUiSlider.on('update', () => {
+            valueElement.value = sliderElement.noUiSlider.get();
+            currentPicture.style.filter = `blur(${`${valueElement.value}px`})`;
+          });
+
+          sliderElement.noUiSlider.updateOptions({
+            range: {
+              min: 0,
+              max: 3,
+            },
+            start: 3,
+            step: 0.1,
+          });
+        } else if (picture.classList[1] === 'effects__preview--heat') {
+          fieldSlider.classList.remove('hidden');
+          sliderElement.noUiSlider.on('update', () => {
+            valueElement.value = sliderElement.noUiSlider.get();
+            currentPicture.style.filter = `brightness(${valueElement.value})`;
+          });
+
+          sliderElement.noUiSlider.updateOptions({
+            range: {
+              min: 1,
+              max: 3,
+            },
+            start: 3,
+            step: 0.1,
+          });
+        } else if (picture.classList[1] === 'effects__preview--none') {
+          fieldSlider.classList.add('hidden');
+          currentPicture.style = '';
+        }
+        console.log(valueElement.value);
       });
     };
 
