1964b30d5d979f74ab0d8a73134eba888e83d148
Юлия К <kazuta.artem@gmail.com>
2023-01-15 23:12:13 +0300

    90%-done-fetch

--- js/fetch-pictures.js
+++ js/fetch-pictures.js
@@ -4,16 +4,25 @@ import { pristine } from './validate-form.js';
 const photoForm = document.querySelector('.img-upload__form');
 const body = document.querySelector('body');
 const photoOverlay = document.querySelector('.img-upload__overlay');
+const buttonUpload = document.querySelector('#upload-submit');
 
 const onSuccess = () => {
   const template = document.querySelector('#success').content;
-  const successMessage = template.querySelector('.success');
+  const successMessage = template.querySelector('.success').cloneNode(true);
   const blockSuccessMessage = successMessage.querySelector('.success__inner');
+  const buttonSuccessMessage = successMessage.querySelector('.success__button');
   body.appendChild(successMessage);
+
+  buttonSuccessMessage.addEventListener('click', () => {
+    successMessage.remove();
+    body.classList.remove('modal-open');
+    photoOverlay.classList.add('hidden');
+  });
+
   successMessage.addEventListener('click', (evt) => {
     const click = evt.composedPath().includes(blockSuccessMessage);
     if (!click) {
-      successMessage.classList.add('hidden');
+      successMessage.remove();
       body.classList.remove('modal-open');
       photoOverlay.classList.add('hidden');
     }
@@ -38,12 +47,16 @@ const sendPictureForm = (closePhoto) => {
     evt.preventDefault();
 
     const isValid = pristine.validate();
+    buttonUpload.disabled = true;
+    buttonUpload.textContent = 'Загружаем фото';
     if (isValid) {
       const formData = new FormData(evt.target);
       fetch('https://25.javascript.pages.academy/kekstagram', {
         method: 'POST',
         body: formData,
       }).then(() => {
+        buttonUpload.disabled = false;
+        buttonUpload.textContent = 'Опубликовать';
         closePhoto();
         onSuccess();
       });
