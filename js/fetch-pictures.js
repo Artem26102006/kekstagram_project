b633f33fabb1adeef8bb09ec5bca865cdc8d55f7
Юлия К <kazuta.artem@gmail.com>
2023-01-15 13:45:31 +0300

    fwefwefwef

--- js/fetch-pictures.js
+++ js/fetch-pictures.js
@@ -2,12 +2,22 @@ import { renderPictures } from './pictures.js';
 import { pristine } from './validate-form.js';
 
 const photoForm = document.querySelector('.img-upload__form');
-const body = document.querySelector('.body');
+const body = document.querySelector('body');
+const photoOverlay = document.querySelector('.img-upload__overlay');
 
 const onSuccess = () => {
   const template = document.querySelector('#success').content;
   const successMessage = template.querySelector('.success');
+  const blockSuccessMessage = successMessage.querySelector('.success__inner');
   body.appendChild(successMessage);
+  successMessage.addEventListener('click', (evt) => {
+    const click = evt.composedPath().includes(blockSuccessMessage);
+    if (!click) {
+      successMessage.classList.add('hidden');
+      body.classList.remove('modal-open');
+      photoOverlay.classList.add('hidden');
+    }
+  });
 };
 
 function getPictures() {
@@ -30,13 +40,10 @@ const sendPictureForm = (closePhoto) => {
     const isValid = pristine.validate();
     if (isValid) {
       const formData = new FormData(evt.target);
-      fetch(
-        'https://25.javascript.pages.academy/kekstagram',
-        {
-          method: 'POST',
-          body: formData,
-        },
-      ).then(() => {
+      fetch('https://25.javascript.pages.academy/kekstagram', {
+        method: 'POST',
+        body: formData,
+      }).then(() => {
         closePhoto();
         onSuccess();
       });
@@ -44,4 +51,4 @@ const sendPictureForm = (closePhoto) => {
   });
 };
 
-export {getPictures, sendPictureForm};
+export { getPictures, sendPictureForm };
